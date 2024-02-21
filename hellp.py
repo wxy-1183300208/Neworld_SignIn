b1fe191171afd65728bc9ea381a2150258ee13a8
wxy-1183300208 <61342207+wxy-1183300208@users.noreply.github.com>
2024-02-21 14:37:22 +0800

    Update hellp.py to adjust latest Selenium

--- hellp.py
+++ hellp.py
@@ -1,52 +1,50 @@
 from selenium import webdriver
 import time
-import json
 import os
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-
-# 虚拟出Chrome界面
+# 设置Chrome选项
 chrome_options = Options()
-chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--window-size=1420,1080')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
-# action  linux服务器驱动地址
-driver = webdriver.Chrome(executable_path='/home/runner/work/Neworld_SignIn/Neworld_SignIn/driver/chromedriver',chrome_options=chrome_options)    # Chrome浏览器  
-# driver = webdriver.Chrome(executable_path='/home/runner/work/Neworld_SignIn/Neworld_SignIn/driver/chromedriver')    # Chrome浏览器  
 
-# windows 系统驱动路径
-# driver = webdriver.Chrome(executable_path='D:\Downloads\chromedriver_win32\chromedriver.exe')    # Chrome浏览器
+# 指定ChromeDriver路径
+chrome_driver_path = '/home/runner/work/Neworld_SignIn/Neworld_SignIn/driver/chromedriver'
+service = Service(executable_path=chrome_driver_path)
 
+# 创建Chrome驱动程序
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# 环境变量中读取数据，包含账号密码，和登陆页面测试
+# 从环境变量中读取数据，包含账号密码和登录页面测试
 u = os.environ["USERNAME"]
 p = os.environ["PASSWORD"]
 
-print('u',u)
-print('p',p)
-driver.get("https://neworld.tv/auth/login") 
-#  获取cookies 
-time.sleep(5)
-# 账号密码登录版本
-driver.find_element_by_id('email').clear()
-driver.find_element_by_id("email").send_keys(u)
+print('u', u)
+print('p', p)
+driver.get("https://neworld.tv/auth/login")
 
-driver.find_element_by_id('passwd').clear()
-driver.find_element_by_id("passwd").send_keys(p)
+# 获取cookies
+time.sleep(5)
+
+# 账号密码登录版本
+driver.find_element(By.ID, 'email').clear()
+driver.find_element(By.ID, "email").send_keys(u)
+
+driver.find_element(By.ID, 'passwd').clear()
+driver.find_element(By.ID, "passwd").send_keys(p)
 
 time.sleep(1)
-driver.find_element_by_id("login").click()
+driver.find_element(By.ID, "login").click()
 
-driver.refresh()#刷新页面 
+# 刷新页面
+driver.refresh()
 
+# 再次刷新页面
+driver.refresh()
 
-driver.refresh()#刷新页面 
-
-# time.sleep(5)
-
-# buttons = driver.find_element_by_xpath("//button[@id='checkin']")
-# print('buttons',buttons)
-
-driver.find_element_by_id("checkin").click() # 点击元素
+# 点击签到按钮
+driver.find_element(By.ID, "checkin").click()
